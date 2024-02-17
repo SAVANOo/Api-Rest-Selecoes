63e797e72517ea3508dd0fb1ee04fadc64059247
SAVANOo <savio_gato1000@hotmail.com>
2024-02-17 16:51:41 -0300

    Refactor: Separando as rotas do servidor

--- src/app.js
+++ src/app.js
@@ -1,14 +1,21 @@
-const express = require('express')
+import express from 'express'
 const app = express()
-const port = 3000
+
+//mock
+const selecoes = [
+    { id: 1, selecao: 'Brasil', grupo: 'g' },
+    { id: 2, selecao: 'Suiça', grupo: 'g' },
+    { id: 3, selecao: 'Servia', grupo: 'g' },
+    { id: 4, selecao: 'Camaroes', grupo: 'g' }
+]
 
 // criar rota padrão
-
 app.get('/', (req, res) => {
     res.send('Hello World')
 })
 
-// escutar a porta
-app.listen(port, () => {
-    console.log(`Servidor rodando no endereço localhost:${port}`)
+app.get('/selecoes', (req, res) => {
+    res.status(200).send(selecoes)
 })
+
+export default app
