d8465057a7eb37b600dfc767eb0d855630db0185
SAVANOo <savio_gato1000@hotmail.com>
2024-02-19 10:06:02 -0300

    Feat: Criando conexao com banco de dados

--- server.js
+++ server.js
@@ -1,7 +1,16 @@
 import app from "./src/app.js"
+import conexao from "./infra/conexao.js"
 const PORT = 3000
 
-// escutar a porta
-app.listen(PORT, () => {
-    console.log(`Servidor rodando no endereço localhost:${PORT}`)
+conexao.connect((error) => {
+    if (error) {
+        console.log(error);
+    } else {
+        console.log('conexao bem sucedida')
+        // escutar a porta
+        app.listen(PORT, () => {
+            console.log(`Servidor rodando no endereço localhost:${PORT}`)
+        })
+    }
 })
+
